0011c49800b0f56d70ebf399162b654dc32ad69e
askor26 <askorutin26@gmail.com>
2023-04-28 21:02:04 +0300

    added swagger

--- apps/api/src/dto/user-dto/create-user-dto.ts
+++ apps/api/src/dto/user-dto/create-user-dto.ts
@@ -1,9 +1,19 @@
 import { IsString, Length, IsEmail } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({
+    example: 'test@gmail.com',
+    description: 'user email',
+  })
   @IsString({ message: 'User email Should be string' })
   @IsEmail({}, { message: 'Incorrect email' })
   readonly email: string;
 
+  @ApiProperty({
+    example: 'password',
+    description: 'user password',
+  })
   @IsString({ message: 'User password Should be string' })
   @Length(4, 16, { message: 'Length should be from 4 to 16' })
   readonly password: string;
