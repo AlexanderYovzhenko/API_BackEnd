c11169b0dcd42a59990b8d1900c7d938f77f684e
askor26 <askorutin26@gmail.com>
2023-04-28 21:03:13 +0300

    changed

---
+++ apps/users/src/interfaces/user.interface.ts
@@ -0,0 +1,4 @@
+export interface UserInterface {
+  email: string;
+  password: string;
+}
--- apps/users/src/users.service.ts
+++ apps/users/src/users.service.ts
@@ -4,16 +4,17 @@ import { User } from './entities/users.entity';
 
 import { CreateUserDto } from '../dto/createUserDto';
 //import { ClientProxy } from '@nestjs/microservices';
+import { UserInterface } from './interfaces/user.interface';
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User) private userRepository: typeof User) {}
 
-  async createUser(userDto: CreateUserDto) {
-    const { email } = userDto;
+  async createUser(newUser: UserInterface) {
+    const { email } = newUser;
     const userExists = await this.getUserByEmail(email);
 
     if (userExists.count === 0) {
-      const user = await this.userRepository.create(userDto);
+      const user = await this.userRepository.create(newUser);
       return user;
     } else {
       throw new HttpException('User already exists', HttpStatus.BAD_REQUEST);
