6b78a97ab9f95045af0b627be13f5b0565a0f161
Alexander <renyreny125tt@gmail.com>
2023-04-29 20:02:55 +0300

    refactor: change import entity User

--- apps/users/src/users.service.ts
+++ apps/users/src/users.service.ts
@@ -1,9 +1,9 @@
 import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from './entities/users.entity';
-
 import { CreateUserDto } from '../dto/createUserDto';
 //import { ClientProxy } from '@nestjs/microservices';
+
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User) private userRepository: typeof User) {}
