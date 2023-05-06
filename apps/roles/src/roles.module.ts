e7e78238589f886a12e9eaadff3b1473d57fec9c
Alexander <renyreny125tt@gmail.com>
2023-05-06 21:44:19 +0300

    rafactor: add role id string

--- apps/roles/src/roles.module.ts
+++ apps/roles/src/roles.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { RolesController } from './roles.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { Role, UsersRoles } from './entities';
+import { Role, UserRole } from './entities';
 import { PostgresDBModule, SharedModule, SharedService } from '@app/shared';
 
 @Module({
+  imports: [
+    SharedModule,
+    PostgresDBModule,
+    SequelizeModule.forFeature([Role, UserRole]),
+  ],
+  controllers: [RolesController],
   providers: [
     RolesService,
     {
@@ -13,12 +19,5 @@ import { PostgresDBModule, SharedModule, SharedService } from '@app/shared';
       useClass: SharedService,
     },
   ],
-  controllers: [RolesController],
-  imports: [
-    SharedModule,
-    PostgresDBModule,
-    SequelizeModule.forFeature([Role, UsersRoles]),
-  ],
-  exports: [RolesService],
 })
 export class RolesModule {}
--- apps/roles/src/roles.service.ts
+++ apps/roles/src/roles.service.ts
@@ -1,31 +1,47 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { Role, UserRole } from './entities';
+import { Repository } from 'sequelize-typescript';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class RolesService {
   constructor(
-    @InjectModel(Role) private roleRepository: typeof Role,
-    @InjectModel(UserRole) private usersRolesRepository: typeof UserRole,
+    @InjectModel(Role) private roleRepository: Repository<Role>,
+    @InjectModel(UserRole) private usersRolesRepository: Repository<UserRole>,
   ) {}
 
+  generateUUID(): string {
+    return uuid();
+  }
+
   async createRole(role) {
-    const newRole = await this.roleRepository.create(role);
+    const newRole = await this.roleRepository.create({
+      role_id: this.generateUUID(),
+      ...role,
+    });
+
     return newRole;
   }
 
   async createUserRole(data) {
-    const newUserRole = await this.usersRolesRepository.create(data);
+    const newUserRole = await this.usersRolesRepository.create({
+      user_role_id: this.generateUUID(),
+      ...data,
+    });
+
     return newUserRole;
   }
 
   async getRoles() {
     const roles = await this.roleRepository.findAll();
+
     return roles;
   }
 
   async getRoleByValue(value: string) {
     const role = await this.roleRepository.findOne({ where: { value } });
+
     return role;
   }
 }
