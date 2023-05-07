6d97261662d6bc014ad3babd88b8f3b142055108
Alexander <renyreny125tt@gmail.com>
2023-05-07 13:55:57 +0300

    refactor: delete dto

--- apps/users/src/users.controller.ts
+++ apps/users/src/users.controller.ts
@@ -1,53 +1,34 @@
+import { Controller, Inject } from '@nestjs/common';
 import {
-  Body,
-  Controller,
-  Get,
-  Post,
-  Param,
-  UsePipes,
-  ValidationPipe,
-  Inject,
-} from '@nestjs/common';
-import {
-  ClientProxy,
   Ctx,
   MessagePattern,
   Payload,
   RmqContext,
 } from '@nestjs/microservices';
-import { CreateUserDto } from './dto/createUserDto';
 import { UsersService } from './users.service';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { SharedService } from '@app/shared';
-import { ContextCreator } from '@nestjs/core/helpers/context-creator';
+import { UserInterface } from './interface/user.interface';
 
-@ApiTags('Endpoints')
-@ApiBearerAuth()
 @Controller()
 export class UsersController {
   constructor(
-    //@Inject('USERS_SERVICE') private readonly usersService: ClientProxy,
     @Inject('SharedServiceInterface')
     private readonly sharedService: SharedService,
     private readonly usersService: UsersService,
   ) {}
 
   @MessagePattern({ cmd: 'create_user' })
-  async createUser(@Ctx() context: RmqContext, @Payload() UserDto) {
-    /* const newUser */
+  async createUser(@Ctx() context: RmqContext, @Payload() user: UserInterface) {
     this.sharedService.acknowledgeMessage(context);
 
-    await this.usersService.createUser(UserDto);
-    // const { name, surname, phone } = userDto;
-    //const profileDto = { name, surname, phone, userID: newUser.id };
-    //this.client.emit('create profile', profileDto);
+    return await this.usersService.createUser(user);
   }
 
   @MessagePattern({ cmd: 'get_all_users' })
-  getUsers(@Ctx() context: RmqContext) {
+  async getUsers(@Ctx() context: RmqContext) {
     this.sharedService.acknowledgeMessage(context);
 
-    return this.usersService.getUsers();
+    return await this.usersService.getUsers();
   }
 
   @MessagePattern({ cmd: 'get_user_by_email' })
@@ -59,6 +40,7 @@ export class UsersController {
     this.sharedService.acknowledgeMessage(context);
 
     const result = await this.usersService.getUserByEmail(email);
-    return result.rows;
+
+    return result;
   }
 }
--- apps/users/src/users.service.ts
+++ apps/users/src/users.service.ts
@@ -1,7 +1,5 @@
-import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
-import { CreateUserDto } from './dto/createUserDto';
-//import { ClientProxy } from '@nestjs/microservices';
 import { UserInterface } from './interface/user.interface';
 import { User } from '@app/shared';
 import { v4 as uuid } from 'uuid';
@@ -11,24 +9,17 @@ import { Repository } from 'sequelize-typescript';
 export class UsersService {
   constructor(@InjectModel(User) private userRepository: Repository<User>) {} // typeof
 
-  generateUUID(): string {
+  private generateUUID(): string {
     return uuid();
   }
 
   async createUser(newUser: UserInterface) {
-    const { email } = newUser;
-    const userExists = await this.getUserByEmail(email);
+    const user = await this.userRepository.create({
+      user_id: this.generateUUID(),
+      ...newUser,
+    });
 
-    if (userExists.count === 0) {
-      const user = await this.userRepository.create({
-        user_id: this.generateUUID(),
-        ...newUser,
-      });
-
-      return user;
-    } else {
-      throw new HttpException('User already exists', HttpStatus.BAD_REQUEST);
-    }
+    return user;
   }
 
   async getUsers() {
@@ -38,10 +29,10 @@ export class UsersService {
   }
 
   async getUserByEmail(email: string) {
-    const result = await this.userRepository.findAndCountAll({
+    const user = await this.userRepository.findOne({
       where: { email },
     });
 
-    return result;
+    return user;
   }
 }
