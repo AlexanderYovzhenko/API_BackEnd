c49b1277c51bf857eb2f42fc132bfc74e8922146
Alexander <renyreny125tt@gmail.com>
2023-05-12 20:50:06 +0300

    refactor: delete not userd

--- apps/person/test/mocks/mock.person.repository.ts
+++ apps/person/test/mocks/mock.person.repository.ts
@@ -1,14 +1,6 @@
 import { personStub } from '../stubs/person.stub';
 
 export const mockPersonRepository = {
-  getPerson: jest
-    .fn()
-    .mockImplementation((person_id: string) =>
-      Promise.resolve(
-        personStub().person_id === person_id ? personStub() : null,
-      ),
-    ),
-
   findOne: jest.fn().mockImplementation(({ where: { person_id } }) => {
     if (typeof person_id !== 'string') {
       return Promise.resolve(personStub());
@@ -18,24 +10,6 @@ export const mockPersonRepository = {
       ? Promise.resolve(personStub())
       : Promise.resolve(null);
   }),
-
-  getAllPersons: jest.fn().mockResolvedValue(Promise.resolve([personStub()])),
-
   findAll: jest.fn().mockResolvedValue(Promise.resolve([personStub()])),
-
-  getPersonsFromFilm: jest
-    .fn()
-    .mockRejectedValue(Promise.resolve([personStub()])),
-
-  getPersonsByName: jest
-    .fn()
-    .mockResolvedValue(Promise.resolve([personStub()])),
-
-  getFilmsByPerson: jest.fn().mockResolvedValue(Promise.resolve(personStub())),
-
-  addPersonsFromFilm: jest
-    .fn()
-    .mockResolvedValue(Promise.resolve([personStub()])),
-
   findOrCreate: jest.fn().mockResolvedValue(Promise.resolve([personStub(), 1])),
 };
